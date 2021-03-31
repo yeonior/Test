1b0fb67f97a3cd6aaf62734d821bded22b4a4c23
Rus <de.mr44@gmail.com>
2021-03-31 23:24:58 +0500

    image picker controller

---
+++ UITableView/NewPlaceViewController.swift
@@ -0,0 +1,77 @@
+//
+//  NewPlaceViewController.swift
+//  UITableView
+//
+//  Created by ruslan on 31.03.2021.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Скрытие лишних разделителей в Table View
+        tableView.tableFooterView = UIView()
+    }
+    
+    // MARK: Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // Убираем клавиатуру по нажатию на все ячейки, кроме нулевой
+        if indexPath.row == 0 {
+            
+            // Добавляем вызов ActionSheet при нажатии на нулевую ячейку
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+
+
+}
+
+// MARK: Text field delegate
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    
+    // Скрываем клавиатуру по нажатию на Done
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+// MARK: Work with image
+
+extension NewPlaceViewController {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+}
